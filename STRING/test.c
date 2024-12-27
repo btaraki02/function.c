33a82a1f4fc38f04367b8f91a0db16469ab79ed6
Taraki Bilal <your.email@example.com>
2024-12-27 12:06:23 +0100

    ko

--- STRING/test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#define momo() printf("hi bilal\n");
-int main(){
-     printf("Error in the line n %d of the file %s\n", __LINE__, __FILE__);
- printf("This file has been compiled on %s at %s\n", __DATE__, __TIME__);
- momo()
-}
